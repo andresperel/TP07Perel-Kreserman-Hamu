5a339bfa6d9eb09395c0c301b7bb4efc3b524170
ANDRES DAVID PEREL <48801689@ort>
2025-08-07 08:55:36 -0300

    h

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP07Perel_Kreserman_Hamu.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
